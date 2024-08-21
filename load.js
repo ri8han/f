67ec3cb456a77dad183670e23eab34797fe56785
ri8han <160643895+ri8han@users.noreply.github.com>
2024-08-21 12:26:28 +0530

    Add files via upload

---
+++ load.js
@@ -0,0 +1,13 @@
+
+document.addEventListener('DOMContentLoaded',(event)=>{
+    setTimeout(() => {
+       
+    const lo=document.getElementById('loading3');
+    const co=document.getElementById('content');
+      
+    lo.style.display='none';
+    co.style.display='block';
+    }, 3000)
+})
+
+
